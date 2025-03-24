4dcc6932b3bc184eeb121f90267f15709b47683e
ylu <123523291+ylu8888@users.noreply.github.com>
2025-03-24 19:26:47 -0400

    Update array.rb

--- array.rb
+++ array.rb
@@ -1,7 +1,7 @@
 class Array
     #make [] return '\0' if out of bounds
   def [](index)
-    super(index) || '\0'  #override ruby built in [] method with super
+    self.fetch(index, '\0') #override ruby built in [] method with super
   end
 
   def map(sequence = nil, &block)
@@ -9,7 +9,7 @@ class Array
     
     if sequence.is_a?(Range)
      #if the argument is a range, check if it is out of bounds
-      if sequence.first < -self.length || sequence.last >= self.length
+      if sequence.first < -self.length #account for negative indices
         return []  #if so, then return an empty array
       end
     end
@@ -50,8 +50,31 @@ class Array
   
 end
 
-a = [1,2,34,5]
-puts a[1] # 2
-puts a[10] # '\0'
-p a.map(2..4) { |i| i.to_f} # [34.0, 5.0]
-p a.map { |i| i.to_f} # [1.0, 2.0, 34.0, 5.0]
+# a = [1,2,34,5]
+# puts a[1] # 2
+# puts a[10] # '\0'
+# p a.map(2..4) { |i| i.to_f} # [34.0, 5.0]
+# p a.map { |i| i.to_f} # [1.0, 2.0, 34.0, 5.0]
+# b = ["cat", "bat", "mat", "sat"]
+# puts b[-1] # "sat"
+# puts b[5] # '\0'
+# p b.map(2..10) { |x| x[0].upcase + x[1,x.length] } # ["Mat", "Sat"]
+# p b.map(2..4) { |x| x[0].upcase + x[1,x.length] } # ["Mat", "Sat"]
+# p b.map(-3..-1) { |x| x[0].upcase + x[1,x.length] } # ["Bat", "Mat",“Sat”]
+# p b.map { |x| x[0].upcase + x[1,x.length] } # ["Cat", "Bat", "Mat","Sat"]
+
+#test cases start here
+b = ["cat", "bat", "mat", "sat"]
+#test case 1
+puts b[-1]  # "sat"
+#test case 2
+puts b[-5]  # '\0' 
+c = [1, 2, 34, 5]
+#test case 3
+p c.map(1..2) { |i| i.to_f }  #[2.0, 34.0]
+#test case 4
+p c.map(3..10) { |i| i.to_f }  #[5.0]
+#test case 5
+p c.map(0) { |i| i.to_f }  #[]
+#test case 6
+p c.map(-2) { |i| i.to_f }  #[]
