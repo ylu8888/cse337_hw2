710c21ae81a1cc72c8a6a2fa77b66c29cf8f0c77
ylu <123523291+ylu8888@users.noreply.github.com>
2025-03-24 23:22:40 -0400

    Create pascal_triangle.rb

---
+++ pascal_triangle.rb
@@ -0,0 +1,25 @@
+def findEncryption(numbers)
+  if numbers.length == 1 #if theres only 1 number left then return it
+    return numbers[0].to_s
+  end
+  
+  while numbers.length > 2  #otherwise we keep looping
+    temp = [] #temporarily store the next iteration of values
+
+    numbers.each_cons(2) do |a, b| #each_cons generates consecutive pairs from array
+      temp << (a + b) % 10 #add the sum and do module 10 to get the last digit
+    end
+
+    numbers = temp #set numbers equal to the temp array
+  end
+  
+  return numbers.join #join all elements into string
+
+end
+
+#Test Cases
+puts findEncryption([66])
+puts findEncryption([8,8,8,8])  
+puts findEncryption([8, 8]) 
+puts findEncryption([1, 2, 3, 4])
+puts findEncryption([8, 1, 4, 8])
